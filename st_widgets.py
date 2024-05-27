3434f7eb78e2327832b08569f35b00070cb38fee
Rasyidannas <rasshit.dsgn@gmail.com>
2024-05-27 15:49:07 +0700

    Widget Part 2: checkbox, radio, select.mp4

--- st_widgets.py
+++ st_widgets.py
@@ -18,3 +18,35 @@ if clicked:
     st.write(':ghost:' * 3)
 
 st.divider()
+
+# Checkbox
+agree = st.checkbox('I agree')
+if agree:
+    # st.write('Great, you  agreed!')
+    'Great, you  agreed!'
+
+checked = st.checkbox('Continue', value=True)
+if checked:
+    ':+1:' * 5
+
+df = pd.DataFrame({
+                    'Name': ['Ciko', 'Ciki', 'Caplin', 'Koski'], 
+                    'Age' : [2, 2, 3, 4]
+                   })
+if st.checkbox('Show data'):
+    st.write(df)
+
+st.divider()
+
+# Radio
+pets = ['cat', 'dog', 'fish', 'turtle']
+pet = st.radio('Favorite pet', pets, index=0, key='your_pet')
+st.write(f'Your favorites pet: {pet}')
+st.write(f'Your favorites pet: {st.session_state.your_pet * 3}')
+
+st.divider()
+
+# Select
+cities = ['London', 'Jakarta', 'Paris', 'Berlin']
+city = st.selectbox('Your city', cities, index=1)
+st.write(f'You life in {city}')
