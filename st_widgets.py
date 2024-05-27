91b4ac5c412749de7e4c6669861ac029dcd0243b
Rasyidannas <rasshit.dsgn@gmail.com>
2024-05-27 16:29:06 +0700

    Widget Part 3: slider, file_uploader, camera_input, image.mp4

--- st_widgets.py
+++ st_widgets.py
@@ -50,3 +50,37 @@ st.divider()
 cities = ['London', 'Jakarta', 'Paris', 'Berlin']
 city = st.selectbox('Your city', cities, index=1)
 st.write(f'You life in {city}')
+
+st.divider()
+
+# Slider
+x = st.slider('x', value=15, min_value=12, max_value=78, step=3)
+st.write(f'x is {x}')
+
+st.divider()
+
+# File Upload
+uploaded_file = st.file_uploader('Upload a file:', type=['txt', 'csv', 'xlsx'])
+if uploaded_file:
+    st.write(uploaded_file)
+    if uploaded_file.type == 'text/plain':
+        from io import StringIO
+        stringio = StringIO(uploaded_file.getvalue().decode('utf-8'))
+        string_data = stringio.read()
+        st.write(string_data)
+    elif uploaded_file.type == 'text/csv':
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
+    else:
+        import pandas as pd
+        df = pd.read_excel(uploaded_file)
+        st.write(df)
+
+st.divider()
+
+# Camera input
+camera_photo = st.camera_input('Take a photo')
+if camera_photo:
+    st.image(camera_photo)
+
+st.image('https://static.streamlit.io/examples/cat.jpg')
